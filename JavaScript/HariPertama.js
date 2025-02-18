09836dae988237d8cb9e31fe5d6b25bb3c4a122c
Frans Guero <antoniirawan798@gmail.com>
2025-02-19 01:52:02 +0700

    arithmetic

---
+++ JavaScript/HariPertama.js
@@ -0,0 +1,55 @@
+// Arithmetic Operators
+let num1 = 2,
+  num2 = 3,
+  num3 = 5,
+  num4 = 3,
+  num5 = 15,
+  num6 = 5;
+
+// + (Addition)
+console.log(10 + 5);
+console.log(num1 + num2);
+
+// - (subtraction)
+console.log(10 - 5);
+console.log(num3 - num1);
+
+// / (division)
+console.log(8 / 2);
+console.log(num3 / num1);
+
+// ** (exponentiation)
+console.log(2 ** 2);
+console.log(num3 ** 2);
+
+// % (modulus)
+console.log(6 % 2);
+console.log(num3 % num1);
+
+// ++ (increment)
+console.log(num1++);
+console.log(num1);
+console.log(++num1);
+
+// -- (decrement)
+console.log(num3--);
+console.log(num3);
+console.log(--num3);
+
+// Menggabungkan dua variabel atau lebih menggunakan += dan -=
+// num1 = 4, num2 = 3, num3 = 3
+num1 += num2; // 3 + 4 = 7
+console.log(num1); // panggil num1 yaitu 7
+
+num2 -= num1; // 3 - 7 = -4
+console.log(num2); // panggil num2 yaitu -4
+
+num3 *= num1; // 3 * 7 = 21
+console.log(num3); // panggil num3 yaitu 21
+
+num3 /= num4; // 21 / 3 = 3
+console.log(num3); // panggil num3 yaitu 3
+
+// num5 = 15, num6 = 5
+num5 /= num6; // 15 / 5 = 3
+console.log(num5); // panggil num5 yaitu 3
