e14ef11c23a0229d8d4afa5dea9e33232262f448
Frans Guero <antoniirawan798@gmail.com>
2025-02-23 23:50:11 +0700

    conditionalstatement

---
+++ JavaScript/conditionalStatement.js
@@ -0,0 +1,18 @@
+let num1 = 10,
+  num2 = 50;
+console.log("Belajar Conditional1");
+console.log(num1);
+console.log(num2);
+if (num1 < num2) {
+  console.log("Benar num1 lebih besar daripada num2");
+} else {
+  console.log("num1 tidak lebih besar daripada num2");
+}
+
+if (num1 < num2) {
+  console.log("Kondisi if benar");
+} else if (num1 == num2) {
+  console.log("kondisi if else benar");
+} else {
+  console.log("Kondisi seluruh if salah");
+}
