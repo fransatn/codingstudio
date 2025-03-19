6a37eb349c5a01b63745f0b596bea327666ee3f4
Frans Guero <antoniirawan798@gmail.com>
2025-03-20 00:34:09 +0700

    js operator

---
+++ JavaScript/logikaoperator.js
@@ -0,0 +1,17 @@
+let var1 = 10,
+  var2 = 30,
+  var3 = 50;
+/*if (10 > 30) {
+  console.log("If Benar");
+} else {
+  console.log("If Salah");
+} */
+if (var2 > var1) {
+  console.log("Benar var2 lebih besar dari var1");
+}
+//true;
+if (var2 > var1 && var2 < var3) {
+  console.log("Benar var2 lebih besar dari var1 dan var2 kecil dari var3");
+} else {
+  console.log("Salah var2 lebih besar dari var1 dan var2 kecil dari var3");
+}
